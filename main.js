b10f31b8328443fd8ddc13e5e1d587b1828efa77
RosaD97 <r960595@gmail.com>
2023-03-13 15:42:31 +0100

    esercizio completato con bonus

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// VueJs
+const { createApp } = Vue
+
+createApp({
+    data(){
+        return{
+        // Messaggio per l'elemento h1
+            message: 'Vue Hello',
+            // Immagine 
+            image: 'https://picsum.photos/id/237/200/300'
+        }
+    }
+
+}).mount('.hello');
